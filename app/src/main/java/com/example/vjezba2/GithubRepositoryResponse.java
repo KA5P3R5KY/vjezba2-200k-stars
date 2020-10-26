3e42b52ad16023c86f7f518f1866d06faba4f588
borna <borna.sunjic.st@gmail.com>
2020-10-26 23:44:45 +0100

    fetch all repositories with more than 100k stars feature done

---
+++ app/src/main/java/com/example/vjezba2/GithubRepositoryResponse.java
@@ -0,0 +1,10 @@
+package com.example.vjezba2;
+
+import java.util.List;
+
+public class GithubRepositoryResponse {
+	public int total_count;
+	public boolean incomplete_results;
+	public List<Item> items;
+}
+
---
+++ app/src/main/java/com/example/vjezba2/License.java
@@ -0,0 +1,9 @@
+package com.example.vjezba2;
+
+public class License {
+	public String key;
+	public String name;
+	public String spdx_id;
+	public String url;
+	public String node_id;
+}
